393b5787f0556f2dbc92242bfe41782ed3c69e13
Maksym-Kartawcew <maxkartavtsev@gmail.com>
2023-10-19 20:47:02 +0200

    first commit

---
+++ src/components/Bounded.js
@@ -0,0 +1,5 @@
+import { Container, InnerContainer } from "./Components.styled";
+
+export function Bounded({ children }) {
+  return <Container>{children}</Container>;
+}
---
+++ src/components/Components.styled.js
@@ -0,0 +1,8 @@
+"use client";
+
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin: 0 auto;
+  width: 1200px;
+`;
